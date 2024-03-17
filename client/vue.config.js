f953af07f00acc66c377d88df5db39c849a3d810
zorro901 <zoro907@gmail.com>
2024-03-17 16:43:17 +0900

    メモリ2GBの制限を解除

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,20 +1,12 @@
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const os=require('os');
-
 module.exports = {
     transpileDependencies: ['vuetify'],
     publicPath: './',
     chainWebpack: config => {
         // ios で reload 時に更新内容が反映されないため
         config.plugins.delete('preload');
-        config
-            .plugin('fork-ts-checker')
-            .tap(args => {
-                let totalmem=Math.floor(os.totalmem()/1024/1024); //get OS mem size
-                let allowUseMem= totalmem>2500? 2048:1000;
-                // in vue-cli shuld args[0]['typescript'].memoryLimit
-                args[0].memoryLimit = allowUseMem;
-                return args
-            })
+        config.plugin("fork-ts-checker").tap((args) => {
+            args[0].memoryLimit = 4096;
+            return args;
+        });
     },
 };
